3f83d25379095c7dd742d0f920b1f484d7914990
Shabeer Moothedath <shabeer.moothedath@maqta.ae>
2022-05-10 12:33:04 +0400

    Asserting Boolean and Null

--- Calculations.Tests/NamesTest.cs
+++ Calculations.Tests/NamesTest.cs
@@ -18,5 +18,22 @@ namespace Calculations.Tests
             Assert.EndsWith("Moothedath", result, StringComparison.InvariantCultureIgnoreCase);
             Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", result);
         }
+
+        [Fact]
+        public void NickName_MustBeNull()
+        {
+            var names = new Names();
+            names.NickName = "Strong Man";
+            Assert.NotNull(names.NickName);
+        }
+
+        [Fact]
+        public void MakeFullName_AlwaysReturnValue()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("Aref", "Karimi");
+            Assert.NotNull(result);
+            Assert.False(string.IsNullOrEmpty(result));
+        }
     }
 }
--- Calculations/Names.cs
+++ Calculations/Names.cs
@@ -6,6 +6,7 @@ namespace Calculations
 {
     public class Names
     {
+        public string NickName { get; set; }
         public string MakeFullName(string firstName, string lastName)
         {
             return $"{firstName} {lastName}";
