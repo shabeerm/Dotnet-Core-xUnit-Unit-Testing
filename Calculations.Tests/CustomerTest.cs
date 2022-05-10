a29dcfd1c603bc0d3823d92897b27ef82985c41c
Shabeer Moothedath <shabeer.moothedath@maqta.ae>
2022-05-10 14:36:16 +0400

    Asserting Range of Values

---
+++ Calculations.Tests/CustomerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Calculations.Tests
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void CheckNameNotEmpty()
+        {
+            var customer = new Customer();
+            Assert.NotNull(customer.Name);
+            Assert.False(string.IsNullOrEmpty(customer.Name));
+        }
+
+        [Fact]
+        public void CheckLegiForDiscount()
+        {
+            var customer = new Customer();
+            Assert.InRange(customer.Age, 25, 40);
+        }
+    }
+}
---
+++ Calculations/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculations
+{
+    public class Customer
+    {
+        public string Name = "Shabeer";
+        public int Age = 30;
+    }
+}
