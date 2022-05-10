2b107e5c10b54bc788f4c87a8eee761c09cc4a8c
Shabeer Moothedath <shabeer.moothedath@maqta.ae>
2022-05-10 15:40:12 +0400

    Class Fixtures to Call depedency globally in test

--- Calculations.Tests/CalculationsTest.cs
+++ Calculations.Tests/CalculationsTest.cs
@@ -5,13 +5,29 @@ using Xunit;
 
 namespace Calculations.Tests
 {
-    public class CalculationsTest
+    public class CalculationsFixture : IDisposable
     {
+        public Calculations calc = new Calculations();
+
+        public void Dispose()
+        {
+            // Clean up resource code can add here
+        }
+    }
+    public class CalculationsTest : IClassFixture<CalculationsFixture>
+    {
+        private readonly CalculationsFixture _calculationsFixture;
+
+        public CalculationsTest(CalculationsFixture calculationsFixture)
+        {
+            this._calculationsFixture = calculationsFixture;
+        }
+
         [Fact]
         [Trait("Category", "Fibo")]
         public void FiboDoesNotIncludeZero()
         {
-            var calc = new Calculations();
+            var calc = _calculationsFixture.calc;
             Assert.All(calc.FiboNumbers, n => Assert.NotEqual(0, n));
         }
 
@@ -19,7 +35,7 @@ namespace Calculations.Tests
         [Trait("Category", "Fibo")]
         public void FiboIncludes13()
         {
-            var calc = new Calculations();
+            var calc = _calculationsFixture.calc;
             Assert.Contains(13, calc.FiboNumbers);
         }
 
@@ -27,7 +43,7 @@ namespace Calculations.Tests
         [Trait("Category", "Fibo")]
         public void FiboDoesNotInclude4()
         {
-            var calc = new Calculations();
+            var calc = _calculationsFixture.calc;
             Assert.DoesNotContain(4, calc.FiboNumbers);
         }
 
@@ -36,7 +52,7 @@ namespace Calculations.Tests
         public void CheckCollection()
         {
             var expectedCollection = new List<int> { 1, 1, 2, 3, 5, 8, 13 };
-            var calc = new Calculations();
+            var calc = _calculationsFixture.calc;
             Assert.Equal(expectedCollection, calc.FiboNumbers);
         }
 
