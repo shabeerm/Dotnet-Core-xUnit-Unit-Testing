51d02f58fab1500372ceb916c7037aed6d4ffa1b
Shabeer Moothedath <shabeer.moothedath@maqta.ae>
2022-05-10 11:34:14 +0400

    Asserting String Values

---
+++ Calculations.Tests/NamesTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Calculations.Tests
+{
+    public class NamesTest
+    {
+        [Fact]
+        public void MakeFullNameTest()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("shabeer", "Moothedath");
+            Assert.Equal("Shabeer Moothedath", result, ignoreCase: true);
+            Assert.Contains("Shabeer", result, StringComparison.InvariantCultureIgnoreCase);
+            Assert.StartsWith("Shabeer", result, StringComparison.InvariantCultureIgnoreCase);
+            Assert.EndsWith("Moothedath", result, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
