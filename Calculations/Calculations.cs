d99b58272e3ee63bb8c9025c30e3cf9661d74601
Shabeer Moothedath <shabeer.moothedath@maqta.ae>
2022-05-10 12:53:40 +0400

    Asserting Collections

---
+++ Calculations/Calculations.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculations
+{
+    public class Calculations
+    {
+        public List<int> FiboNumbers => new List<int> { 1, 1, 2, 3, 5, 8, 13 };
+    }
+}
